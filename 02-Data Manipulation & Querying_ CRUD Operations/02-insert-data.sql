334102540f8a79f76e2d4e11d6b1bf2cfaaeecea
milanr21 <milanrawal309@gmail.com>
2022-05-14 15:08:28 +0545

    Data Manipulation and CRUD operation

---
+++ 02-Data Manipulation & Querying_ CRUD Operations/02-insert-data.sql
@@ -0,0 +1,37 @@
+-- INSERT INTO sales (
+--     customer_name,
+--     product_name,
+--     volume,
+--     is_recurring
+--   )
+-- VALUES (
+--   'Max Schwarz',
+--   'A Book',
+--   12.99,
+--   TRUE
+-- );
+
+
+-- INSERT INTO sales (
+--     data_fulfilled,
+--     customer_name,
+--     product_name,
+--     volume,
+--     is_recurring,
+--     is_disputed
+--   )
+-- VALUES (
+--   NULL,
+--   'Learning Inc',
+--   'Course Bundle',
+--   4889.62,
+--   FALSE,
+--   FALSE
+-- ), (
+--   '2022-04-10',
+--   'Big Oil Inc',
+--   'Trucks',
+--   400000.0,
+--   FALSE,
+--   TRUE
+-- );
---
+++ 02-Data Manipulation & Querying_ CRUD Operations/07-select-data.sql
@@ -0,0 +1,4 @@
+-- SELECT * FROM sales;
+
+SELECT 
+customer_name, product_name, volume/1000, date_created FROM sales;
