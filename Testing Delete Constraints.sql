33ca0afc5eb8fb79fdf94cef6a7f8a71af1ee751
Milan Rawal <67459437+milanr21@users.noreply.github.com>
2022-12-13 17:07:11 +0545

    Create Testing Delete Constraints.sql

---
+++ Testing Delete Constraints.sql
@@ -0,0 +1,84 @@
+-- CREATE TABLE users (
+-- 	id SERIAL PRIMARY KEY,
+--   username VARCHAR(50)
+
+-- );
+
+-- INSERT INTO users (username)
+-- VALUES ('milan'), ('monika'), ('rahul'), ('ram');
+
+-- CREATE TABLE photos (
+-- id SERIAL PRIMARY KEY,
+-- url VARCHAR(200),
+--  user_id INTEGER REFERENCES users(id)
+-- );
+
+-- INSERT INTO photos (url, user_id)
+-- VALUES ('http://one.jpg', 4);
+
+
+-- INSERT INTO photos(url, user_id)
+-- VALUES ('http://two.jpg', 1),
+-- ('http://25.jpg', 1),
+-- ('http://36.jpg', 1),
+-- ('http://745.jpg', 2),
+-- ('http://35.jpg', 3),
+-- ('http://256.jpg', 4);
+
+
+-- SELECT * FROM photos
+-- WHERE user_id = 4;
+
+
+-- SELECT url, username FROM photos
+-- JOIN users on users.id = photos.user_id;
+
+-- DELETE FROM users 
+-- WHERE id = 1;
+
+-- DROP TABLE photos;
+
+-- CREATE TABLE photos (
+-- id SERIAL PRIMARY KEY,
+-- url VARCHAR(200),
+--  user_id INTEGER REFERENCES users(id) ON DELETE SET NULL
+-- );
+
+-- INSERT INTO photos (url, user_id)
+-- VALUES ('http://one.jpg', 4);
+
+
+-- INSERT INTO photos(url, user_id)
+-- VALUES ('http://two.jpg', 1),
+-- ('http://25.jpg', 1),
+-- ('http://36.jpg', 1),
+-- ('http://745.jpg', 2),
+-- ('http://35.jpg', 3),
+-- ('http://256.jpg', 4);
+
+
+
+-- SELECT * FROM photos;
+
+-- DELETE FROM users 
+-- WHERE id = 1;
+
+
+-- INSERT INTO photos(url, user_id)
+-- VALUES 
+-- ('http://745.jpg', 2),
+-- ('http://35.jpg', 3),
+-- ('http://256.jpg', 4);
+
+
+-- DELETE FROM users
+-- WHERE id = 4;
+
+-- SELECT * FROM photos;
+
+
+
+
+
+
+
