a6d741eed6c9323d21f580dd0eb990864b10f641
Milan Rawal <67459437+milanr21@users.noreply.github.com>
2022-12-22 18:15:34 +0545

    Create limit and offset.sql

---
+++ limit and offset.sql
@@ -0,0 +1,58 @@
+-- SELECT *
+-- FROM products
+-- ORDER BY price ASC;
+
+
+-- SELECT *
+-- FROM products
+-- ORDER BY price DESC;
+
+-- SELECT * FROM products order BY name;
+
+-- SELECT * 
+-- FROM products
+-- ORDER BY price,weight;
+
+
+-- SELECT *
+-- FROM users
+-- OFFSET 40;
+
+
+-- SELECT *
+-- FROM products
+-- order BY price asc
+-- LIMIT 5;
+
+-- SELECT *
+-- FROM products
+-- order BY price DESC
+-- LIMIT 5;
+
+
+-- SELECT *
+-- FROM products
+-- order BY price DESC
+-- LIMIT 5
+-- OFFSET 1;
+
+
+SELECT *
+FROM phones
+order BY price DESC
+LIMIT 2
+OFFSET 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
